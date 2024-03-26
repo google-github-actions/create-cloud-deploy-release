d8273ca82ea55404df66c9842b89579b51693349
Luke Barge <lukebarge@gmail.com>
2024-03-26 10:00:03 +0000

    Fixed unset deploy paramaters

--- tests/unit/main.test.ts
+++ tests/unit/main.test.ts
@@ -362,6 +362,16 @@ test('#run', { concurrency: true }, async (suite) => {
     ]);
   });
 
+  await suite.test('does not set deploy parameters if not given', async (t) => {
+    const mocks = defaultMocks(t.mock);
+
+    await run();
+    
+    const execArguments = mocks.getExecOutput.mock.calls?.at(0)?.arguments?.at(1);
+    assert(execArguments);
+    assert(!execArguments?.includes('--deploy-parameters'));
+});
+
   await suite.test('sets flags if given', async (t) => {
     const mocks = defaultMocks(t.mock, {
       flags: '--flag1=value1 --flag2=value2',
